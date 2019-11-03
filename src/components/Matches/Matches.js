6c8af32fbc2f8c7a0932e44b8eb365bd6c0497c2
Philip Khaisman <pkhaisman@gmail.com>
2019-11-03 12:58:47 -0500

    Prepare app for static client submission

--- src/components/Matches/Matches.js
+++ src/components/Matches/Matches.js
@@ -12,7 +12,7 @@ class Matches extends React.Component {
         return this.context.currentUser.usersMatched
             ? this.context.currentUser.usersMatched.map(matchId => {
                 const matchName = this.context.users.map(user => {
-                    if (user.id === matchId) {
+                    if (user.id === matchId && this.context.currentUser.id !== matchId) {
                         return (
                             <div className='Match__name' key={matchId}>{user.firstName}</div>
                         )
@@ -33,8 +33,8 @@ class Matches extends React.Component {
     render() {
         return (
             <div className='Matches'>
-                <button onClick={(e) => this.context.handleMockUserLogin(this.context.users[0])}>Phil</button>
-                <button onClick={(e) => this.context.handleMockUserLogin(this.context.users[1])}>Mike</button>
+                {/* <button onClick={(e) => this.context.handleMockUserLogin(this.context.users[0])}>Phil</button> */}
+                {/* <button onClick={(e) => this.context.handleMockUserLogin(this.context.users[1])}>Mike</button> */}
                 {this.renderMatches()}
             </div>
         )
