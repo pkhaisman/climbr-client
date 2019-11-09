55b4bfe3d7aa4dcaa028023f6842cc5b0a6b0e52
Philip Khaisman <pkhaisman@gmail.com>
2019-11-09 12:00:08 -0500

    Prepare for MVP submission

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,55 +4,29 @@ import TokenService from '../../services/token-service'
 import './NavBar.css'
 
 class NavBar extends React.Component {
-    renderLoginLink = () => {
-        return (
-            <li>
-                <Link to={'/login'}>
-                    Log In
-                </Link>
-            </li>
-        )
-    }
-
     handleLogoutClick = (e) => {
         TokenService.clearAuthToken()
         this.props.history.push('/login')
         this.props.updateUserLoggedIn(false)
     }
 
-    renderLogoutLink = () => {
-        return (
-            // <li>
-            //     <Link to={'/'} onClick={(e) => this.handleLogoutClick(e)}>
-            //         Log Out
-            //     </Link>
-            // </li>
-            <li>
-                <button onClick={(e) => this.handleLogoutClick(e)}>
-                    Log Out
-                </button>
-            </li>
-        )
-    }
-
-    renderLoginOrLogout = () => {
-        return TokenService.hasAuthToken()
-            ? this.renderLogoutLink()
-            : this.renderLoginLink()
-    }
-
     render() {
-        return (
-            <nav className='NavBar'>
+        return TokenService.hasAuthToken() 
+            ? <nav className='NavBar'>
                 <ul className='NavBar__list'>
-                    <li><Link to={'/signup'}>Sign Up</Link></li>
-                    {this.renderLoginOrLogout()}
                     <li><Link to={'/profile'}>Profile</Link></li>
                     <li><Link to={'/swipe'}>Swipe</Link></li>
                     <li><Link to={'/match'}>Matches</Link></li>
+                    <li><button onClick={(e) => this.handleLogoutClick(e)}>Log Out</button></li>
                 </ul>
             </nav>
-        )
+            : <nav className='NavBar'>
+                <ul className='NavBar__list'>
+                    <li><Link to={'/'}>Landing Page</Link></li>
+                    <li><Link to={'/login'}>Log In</Link></li>
+                    <li><Link to={'/signup'}>Sign Up</Link></li>
+                </ul>
+            </nav>
     }
 }
 
--- src/components/Pages/LandingPage/LandingPage.js
+++ src/components/Pages/LandingPage/LandingPage.js
@@ -6,6 +6,15 @@ function LandingPage() {
     return (
         <div className='LandingPage'>
             <NavBar />
+            <h1>Find partners to climb with!</h1>
+            <ul>
+                <h2>Demo Login Info</h2>
+                <li>username: demo@gmail.com</li>
+                <li>password: mpass</li>
+            </ul>
+            <p>Click on the 'Swipe' link to swipe through users</p>
+            <p>Click on the 'Match' link to see a list of users you have matched with. For the demo account every user has already liked you so any user you swipe right on, you will match with</p>
+            <p>Click on a match to open a chat with them</p>
         </div>
     )
 }
