1696640553609629e48c8df395a97f7ca9219f32
Philip Khaisman <pkhaisman@gmail.com>
2019-11-15 15:21:42 -0500

    Change endpoint to heroku server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
-    // API_ENDPOINT: 'https://fierce-harbor-75048.herokuapp.com/api',
+    // API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://fierce-harbor-75048.herokuapp.com/api',
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'climbr-token-key'
 }
